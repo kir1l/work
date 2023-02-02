dbb12425272c4103217242b6ee73d03585e4c884
Kirill Balushin <balusinkirill@gmail.com>
2023-02-02 19:01:50 +0300

    fix page height + some changes

--- gulpfile.js
+++ gulpfile.js
@@ -10,14 +10,11 @@ const gulp = require('gulp'), // основной модуль
       sourcemaps = require('gulp-sourcemaps'),
       image = require('gulp-tinypng-compress'),
       browsersync = require('browser-sync').create(),
-      fs = require('fs'),
-      fonter = require('gulp-fonter'),
-      ttf2woff2 = require('gulp-ttf2woff'),
       img2webp = require('gulp-webp'),
       newer = require('gulp-newer'),
       sftp = require('gulp-sftp'),
       svgmin = require('gulp-svgmin'),
-      pathModule = require('path');
+	  webp = require('gulp-webp')
 
 // пути
 const path = {
@@ -38,12 +35,12 @@ const path = {
 	},
 	// пути к картинкам
 	img: {
-		src: 'src/assets/images/**/*.{png,jpg,jpeg,webp,gif}',
+		src: 'src/assets/images/*.{png,jpg,jpeg,webp,gif}',
 		dest: 'dist/assets/images/',
 	},
 	// пути к шрифтам
 	fonts: {
-		src: 'src/assets/fonts/',
+		src: 'src/assets/fonts/*',
 		dest: 'dist/assets/fonts/',
 	},
 	// путь к svg
@@ -123,12 +120,12 @@ function img() {
 	//prettier-ignore
 	return gulp.src(path.img.src)
     .pipe(newer(path.img.dest))
+	.pipe(webp())
     .pipe(image({
         key: 'fPv2DXfQrCWJSzLR8wbSW3CfNmhNTYVg',
         sigFile: 'images/.tinypng-sigs',
         log: true
     }))
-    .pipe(img2webp())
     .pipe(gulp.dest(path.img.dest))
 }
 
@@ -141,79 +138,9 @@ function svg() {
 
 // =============== шрифты =============
 
-function otftottf() {
-	return gulp
-		.src(path.fonts.src)
-		.pipe(
-			fonter({
-				formats: ['ttf'],
-			})
-		)
-		.pipe(gulp.src(path.fonts.src));
-}
-
-function ttftowoff() {
-	return gulp
-		.src(path.fonts.src)
-		.pipe(
-			fonter({
-				formats: ['woff'],
-			})
-		)
+function fonts() {
+	return gulp.src(path.fonts.src)
 		.pipe(gulp.dest(path.fonts.dest))
-		.pipe(gulp.src('src/assets/fonts/*.ttf'))
-		.pipe(ttf2woff2())
-		.pipe(gulp.dest(path.fonts.dest));
-}
-
-function fontsStyle(cb) {
-	//Файл стилей подключения шрифтов
-	const fontsFile = `src/components/scss/fonts.scss`;
-	//Проверяем, существуют ли файлы шрифтов
-	const fontFiles = fs.readdirSync(path.fonts.dest);
-	const fontWeights = {
-		thin: 100,
-		extralight: 200,
-		light: 300,
-		medium: 500,
-		semibold: 600,
-		bold: 700,
-		extrabold: 800,
-		heavy: 800,
-		black: 900,
-		default: 400,
-	};
-	const fileNames = [];
-
-	for (let i = 0; i < fontFiles.length; i++) {
-		const filePath = fontFiles[i];
-		const fontFileName = pathModule.basename(
-			filePath,
-			pathModule.extname(filePath)
-		);
-
-		if (fileNames.indexOf(fontFileName) !== -1) continue;
-		fileNames.push(fontFileName);
-
-		const fileNameParts = fontFileName.split('-');
-		// игнорируем все файлы, имя которых не соответсвует маске: FontName-FontWeight
-		if (fileNameParts.length !== 2) continue;
-
-		const fontName = fileNameParts[0];
-		const fontWeightPart =
-			typeof fileNameParts[1] === 'string'
-				? fileNameParts[1].toLowerCase()
-				: fileNameParts[1];
-
-		const fontWeight = fontWeights[fontWeightPart] || fontWeights.default;
-
-		fs.appendFileSync(
-			fontsFile,
-			`@font-face{\n\tfont-family: ${fontName};\n\tfont-display: swap;\n\tsrc: url("../assets/fonts/${fontFileName}.woff2") format("woff2"), url("../assets/fonts/${fontFileName}.woff") format("woff");\n\tfont-weight: ${fontWeight};\n\tfont-style: normal;\n}\r\n`
-		);
-	}
-
-	cb();
 }
 
 // ================ sftp
@@ -242,30 +169,20 @@ function watch() {
 	gulp.watch('src/components/scss/*.scss', stylesDev);
 	gulp.watch('src/components/js/*.js', scripts);
 	gulp.watch('src/components/html/*.html', html);
+	gulp.watch(path.img.src, img);
+	gulp.watch(path.svg.src, svg);
 }
-
-const info = () => {
-	console.log(
-		'comands: \n gulp prod: окончательная сборка проекта со сжатием всех файлов \n gulp dev / gulp: классическая сборка проекта \n gulp fonts: автоматическая конвертация и запись в стили шрифтов \n gulp sftp: передача фалов на удаленный сервер в настройках надо вписать данные.\u001b[33m ВНИМАНИЕ!\u001b[0m не забывайте удалять ключи при публикации кода в открытый доступ'
-	);
-};
 
 // dev и prod
 exports.prod = gulp.series(
 	clean,
-	otftottf,
-	ttftowoff,
-	fontsStyle,
-	gulp.parallel(scripts, stylesProd, html, img, svg)
+	gulp.parallel(scripts, stylesProd, fonts, html, img, svg)
 );
 exports.dev = gulp.series(
 	clean,
-	otftottf,
-	ttftowoff,
-	fontsStyle,
-	gulp.parallel(scripts, stylesDev, html, img, svg),
+	gulp.parallel(scripts, stylesDev, fonts, html, img, svg),
 	watch
 );
-exports.default = this.dev;
+module.exports.default = this.dev;
 exports.sftp = sftprun;
-exports.info = info;
+exports.fonts = fonts;
